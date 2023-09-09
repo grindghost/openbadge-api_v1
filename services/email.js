91903d9434f478a93638726c79625d1f08e36f62
GuillaumePB <55443646+GuillaumePB@users.noreply.github.com>
2023-09-09 14:00:35 -0400

    Removed the writing of the file

--- services/email.js
+++ services/email.js
@@ -13,9 +13,6 @@ const nodemailer = require("nodemailer");
 // async..await is not allowed in global scope, must use a wrapper
 async function SendEmail(recipientemail, imgbuffer, badgename, downloadurl, recipient) {
 
-  // Save the file and attach it to the email
-  fs.writeFileSync('badge_embeded.png', imgbuffer);
-
   // Dynamic replacement for handlebars, in the html template...
   const replacements = {
     badgeimg_cid: 'badgeimg_cid',
@@ -51,8 +48,7 @@ async function SendEmail(recipientemail, imgbuffer, badgename, downloadurl, reci
     html: htmlToSend, // html body
     attachments: [{
       filename: 'badge.png',
-      // content: imgbuffer,
-      path: 'badge_embeded.png',
+      content: imgbuffer,
       encoding: 'binary',
       cid: 'badgeimg_cid' //same cid value as in the html img src
     },
@@ -65,9 +61,6 @@ async function SendEmail(recipientemail, imgbuffer, badgename, downloadurl, reci
 
   console.log("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Clean the file after sending the email
-  fs.unlinkSync('badge_embeded.png');
 }
 
 // SendEmail().catch(console.error);
